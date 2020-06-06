795e027ce00bd136c87e56ed101ee280b9a672b6
克桥 韦 <1264251638@qq.com>
2020-06-06 10:20:49 +0800

    <refeat>重写SendRequest中的request方法

--- lib/utils/SendRequest.dart
+++ lib/utils/SendRequest.dart
@@ -11,72 +11,34 @@ var ip = "139.155.151.126:6781";
 
 Future<dynamic> request(uri, method, header, param, body, context) async {
   print("进入request");
+  // 创建请求客户端
+  HttpClient httpClient = new HttpClient();
+
+  // 请求资源定位
+  var url = Uri.http(ip, uri, param);
+
+  // 请求实体
+  HttpClientRequest request = null;
   if (method == "GET") {
-    return getRequest(uri, header, param, body, context);
+    request = await httpClient.getUrl(url);
   } else {
-    return postRequest(uri, header, param, body, context);
+    request = await httpClient.postUrl(url);
+
+    // 添加请求体
+    request.add(utf8.encode(json.encode(body)));
   }
-}
-
-Future<dynamic> getRequest(path, header, param, body, context) async {
-  print("进入get");
-  var httpClient = new HttpClient();
-
-  var url = Uri.http(ip, path, param);
-  var request = await httpClient.getUrl(url);
-
-  // 添加请求头
-  request.headers.add("Content-type", "application/json");
-
-  var response = await request.close();
-
-  // 返回码
-  var resCode = response.statusCode;
-
-  var responseBody = await response.transform(utf8.decoder).join();
-  if (resCode == 200) {
-    var res = json.decode(responseBody);
-
-    if (res["code"] == 200){
-      return responseBody;
-    }
-    else{
-      myToast(res["msg"]);
-    }
-  } else {
-    myToast("网络错误，请重试");
-  }
-  return null;
-}
-
-Future<dynamic> postRequest(path, header, param, body, context) async {
-  print("进入post方法");
-
-  var httpClient = new HttpClient();
-  var url = Uri.http(ip, path, param);
 
   // 构建请求头
   String userId = '';
   String token = '';
 
   await getStorage('user_id').then((value) => {userId = value});
-
   await getStorage('token').then((t) => {token = t});
-
-  Map<String, String> header = {'user_id': userId.toString(), 'token': token};
-
-  print(header['user_id']);
-  print(header['token']);
-
-  var request = await httpClient.postUrl(url);
 
   // 添加请求头 header
   request.headers.add("Content-type", "application/json");
   request.headers.add("user_id", userId);
   request.headers.add("token", token);
-
-  // 添加请求体 data/body
-  request.add(utf8.encode(json.encode(body)));
 
   var response = await request.close();
 
